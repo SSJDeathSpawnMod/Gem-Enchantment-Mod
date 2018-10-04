ba7212339156c0c0dd9f1e2dedff02e16221e3f9
SSJDeathSpawnMod <deathspawn28@gmail.com>
2018-10-04 23:19:06 +0530

    fluids...

--- src/main/java/com/deathspawn/advanced/init/ModFluids.java
+++ src/main/java/com/deathspawn/advanced/init/ModFluids.java
@@ -13,6 +13,6 @@ public class ModFluids {
 	
 	public static List<Fluid> FLUIDS = new ArrayList<Fluid>();
 	
-	public static Fluid enchantedFluid = new FluidEnchantedLiquid("enchanted", new ResourceLocation(Reference.MOD_ID, "fluids/enchanted_still"), new ResourceLocation(Reference.MOD_ID, "fluids/enchanted_flow")).setMaterial(ModMaterials.ENCHANTED).setDensity(100).setGaseous(false).setLuminosity(2).setViscosity(25000).setTemperature(300);
+	public static Fluid enchantedFluid = new FluidEnchantedLiquid("enchanted", new ResourceLocation(Reference.MOD_ID, "enchanted_still"), new ResourceLocation(Reference.MOD_ID, "enchanted_flow")).setMaterial(ModMaterials.ENCHANTED).setDensity(100).setGaseous(false).setLuminosity(2).setViscosity(25000).setTemperature(300);
 	
 }
